755286363a77c96f483de21ee80cce69f1bc079c
FIRQY <firqy.sutan@gmail.com>
2023-08-31 15:08:41 +0700

    Update Composer

--- resources/views/admin/transaction/show.blade.php
+++ resources/views/admin/transaction/show.blade.php
@@ -1,3 +1,54 @@
+<style>
+    .display-none {
+        display: none !important;
+    }
+
+    .overlay {
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100vh;
+        background: rgba(0, 0, 0, .8);
+        z-index: 999;
+        opacity: 1;
+        transition: all 0.5s;
+    }
+
+
+    .lds-dual-ring {
+        display: inline-block;
+    }
+
+    .lds-dual-ring:after {
+        content: " ";
+        display: block;
+        width: 64px;
+        height: 64px;
+        margin: 5% auto;
+        border-radius: 50%;
+        border: 6px solid #fff;
+        border-color: #fff transparent #fff transparent;
+        animation: lds-dual-ring 1.2s linear infinite;
+    }
+
+    @keyframes lds-dual-ring {
+        0% {
+            transform: rotate(0deg);
+        }
+
+        100% {
+            transform: rotate(360deg);
+        }
+    }
+
+    #getDataBtn {
+        background: #e2e222;
+        border: 1px solid #e2e222;
+        padding: 10px 20px;
+    }
+</style>
+
 <!-- Modal -->
 <div class="modal fade" id="modal-edit" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-lg">
@@ -34,9 +85,11 @@
     </div>
 </div>
 
+<div id="loader" class="lds-dual-ring display-none overlay"></div>
+
 <script>
     //button create post event
-    $('body').on('click', '.btn-show-post', function () {
+    $('.btn-show-post').click(function () {
 
         // let invoice_no = $(this).data('id');
         let url_show = $(this).data('url');
@@ -47,14 +100,19 @@
             url: url_show,
             type: "GET",
             cache: false,
+            beforeSend: function () {
+                $('#loader').removeClass('display-none')
+            },
             success:function(response){
-
                 //fill data to form
                 $('#invoice_no').val(response.transaction.invoice_no);
 
                 //open modal
                 $('#modal-edit').modal('show');
-            }
+            },
+            complete: function () {
+                $('#loader').addClass('display-none')
+            },
         });
     });
 
