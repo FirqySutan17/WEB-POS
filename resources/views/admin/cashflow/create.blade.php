533402ddcd37a0c74cabb981a3138ef35be4fd74
FIRQY <firqy.sutan@gmail.com>
2023-09-08 16:19:15 +0700

    Added Cashflow CRUD

--- resources/views/admin/cashflow/create.blade.php
+++ resources/views/admin/cashflow/create.blade.php
@@ -209,5 +209,31 @@ CMS | Add Cashflow
             // }
         });
     });
+
+    /* Tanpa Rupiah */
+        var tanpa_rupiah    = document.getElementById('input_user_cash');
+        tanpa_rupiah.addEventListener('keyup', function(e) {
+            var nominal = this.value;
+            var nominal_number = Number(nominal.replace(".", ""));
+            tanpa_rupiah.value = formatRupiah(nominal);
+        });
+
+        /* Fungsi */
+        function formatRupiah(angka, prefix)
+        {
+            var number_string = angka.replace(/[^,\d]/g, '').toString(),
+                split    = number_string.split(','),
+                sisa     = split[0].length % 3,
+                rupiah     = split[0].substr(0, sisa),
+                ribuan     = split[0].substr(sisa).match(/\d{3}/gi);
+                
+            if (ribuan) {
+                separator = sisa ? '.' : '';
+                rupiah += separator + ribuan.join('.');
+            }
+            
+            rupiah = split[1] != undefined ? rupiah + ',' + split[1] : rupiah;
+            return prefix == undefined ? rupiah : (rupiah ? 'Rp. ' + rupiah : '');
+        }
 </script>
 @endpush
