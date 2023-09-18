41a4e5629d0802795fd6cf9e572d938423190e91
FIRQY <firqy.sutan@gmail.com>
2023-09-18 16:43:14 +0700

    Asset local

--- resources/views/admin/authentication/partials/js.blade.php
+++ resources/views/admin/authentication/partials/js.blade.php
@@ -11,6 +11,6 @@
 @stack('scripts')
 <!-- Plugins JS Ends-->
 <!-- Theme js-->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
+<script src="{{asset('assets/js/bootstrap.bundle.min.js')}}"></script>
 <script src="{{asset('assets/js/script.js')}}"></script>
 <!-- Plugin used-->
